697b3fa9aba8ae47cd1111887d88662e658ebc2d
91940 <91940@Aswin94>
2023-07-19 20:41:47 +0530

    Committing excel read

---
+++ ReadExcel/src/main/java/packsample/Excel_Main.java
@@ -0,0 +1,15 @@
+package packsample;
+
+import java.io.IOException;
+
+public class Excel_Main {
+
+	public static void main(String[] args) throws IOException {
+		String s=Excel_new.getStringData(0, 1);
+		System.out.println(s);
+		String n= Excel_new.getIntegerData(0, 0);
+		System.out.println(n);
+
+	}
+
+}
